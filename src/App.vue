a5061915c41527ad56e53d3338dbfaf1c354ef0e
Andrea Damiani <andrea.dam90@gmail.com>
2023-05-10 17:40:36 +0200

    Aggiunta schermata Mobile

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-    <main class="flex h-screen">
+    <!-- Schermata Mobile -->
+    <div
+        v-if="width <= 1024 && orientation === 'portrait-primary'"
+        class="flex h-screen w-screen flex-col items-center justify-center gap-5 bg-slate-900 p-5 text-white">
+        <h1 class="text-center text-5xl">Tailwind Colors</h1>
+        <h2 class="text-2xl italic">by Andrea Damiani</h2>
+        <Icon icon="material-symbols:mobile-off-rounded" class="text-9xl" />
+        <h2 class="text-center text-3xl">Quest'app è utilizzabile solo su uno schermo grande.</h2>
+    </div>
+
+    <main v-else class="flex h-screen">
         <!-- Slate -->
         <div class="grid h-full w-96">
             <ColorSquare class="bg-slate-50" to-be-copied="#f8fafc">#f8fafc</ColorSquare>
@@ -333,5 +343,11 @@
 </template>
 
 <script setup>
+import { useWindowSize, useScreenOrientation } from "@vueuse/core";
+import { Icon } from "@iconify/vue";
+
 import ColorSquare from "./components/ColorSquare.vue";
+
+const { width } = useWindowSize();
+const { orientation } = useScreenOrientation();
 </script>
