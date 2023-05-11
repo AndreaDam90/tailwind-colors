5356b4aef6884e124617e8a6fb01f1667d61a594
Andrea Damiani <andrea.dam90@gmail.com>
2023-05-11 11:26:37 +0200

    Completamente ristrutturata app

---
+++ src/components/ShadeGrid.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="grid w-96">
+        <ColorSquare
+            v-for="(gradient, index) in gradients"
+            :key="index"
+            :class="`bg-${tailwindClass}-${gradient}`"
+            :to-be-copied="shades[index]"
+            >{{ shades[index] }}</ColorSquare
+        >
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import ColorSquare from "./ColorSquare.vue";
+
+defineProps(["tailwindClass", "shades"]);
+
+const gradients = ref(["50", "100", "200", "300", "400", "500", "600", "700", "800", "900", "950"]);
+</script>
