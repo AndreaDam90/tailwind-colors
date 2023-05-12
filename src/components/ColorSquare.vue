eab1e1d2701b0ba1661313e44e091bb6b4178727
Andrea Damiani <andrea.dam90@gmail.com>
2023-05-12 13:11:43 +0200

    Installata Icona nel progetto

--- src/components/ColorSquare.vue
+++ src/components/ColorSquare.vue
@@ -2,7 +2,7 @@
     <button class="group relative" @click="copy(toBeCopied)" :disabled="copied">
         <div
             class="absolute bottom-0 top-0 flex w-full items-center justify-center bg-neutral-200/20 font-semibold opacity-0 transition-opacity duration-100 ease-in group-hover:opacity-100">
-            <Icon v-if="copied" icon="material-symbols:done-rounded" class="text-4xl" />
+            <Icon v-if="copied" :icon="doneRounded" class="text-4xl" />
             <slot v-else></slot>
         </div>
     </button>
@@ -10,7 +10,9 @@
 
 <script setup>
 import { useClipboard } from "@vueuse/core";
-import { Icon } from "@iconify/vue";
+
+import { Icon } from '@iconify/vue';
+import doneRounded from '@iconify-icons/material-symbols/done-rounded';
 
 defineProps(["toBeCopied"]);
 
